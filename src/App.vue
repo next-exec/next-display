8f6c8df2219037428dec1915150c33831f50f464
Leo Yao <45076643+leoccyao@users.noreply.github.com>
2022-04-07 16:23:37 -0400

    Minimum viable events product

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container-fluid" style="height: 98vh; padding-top: 2vh; padding-left: 2vw;">
     <div class="row mb-4" style="height: 85%;">
-      <div class="col-md-7" style="height: 100%; display: flex; flex-direction: column">
+      <div class="col-md-8" style="height: 100%; display: flex; flex-direction: column">
         <h1 class="mb-2">Announcements <span class="material-icons">announcement</span></h1>
         <div style="height: 100%; display: flex; flex-direction: column; justify-content: center">
         <p class="ml-2 mr-2" style="white-space: pre-line" v-if="currentAnnouncement.type == 'text'">{{ currentAnnouncement.data }}</p>
@@ -10,7 +10,7 @@
         <!--<div class="mt-4" style="background-repeat: no-repeat; background-size: contain; flex: 1;" :style="{'background-image': 'url(' + currentImage + ')'}">
         </div>-->
       </div>
-      <div class="col-md-5" style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; align-items: center;">
+      <div class="col-md-4" style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; align-items: center;">
         <div>
           <h1>Weather <span class="material-icons">cloud</span></h1>
           <img :src="'http://openweathermap.org/img/w/' + weather.icon + '.png'" style="width: 25%;">
@@ -19,15 +19,19 @@
         </div>
         <div>
           <h1>Shuttles <span class="material-icons">airport_shuttle</span></h1>
-          <h2 v-if="info.size === 0">No shuttles are currently being tracked.</h2>
+          <h3 v-if="info.size === 0">No shuttles are currently being tracked.</h3>
           <table>
             <tbody>
               <tr v-for="item in info.values()" v-bind:key="item.title">
-                <td style="text-align: left;"><h2>{{ item.title }}</h2></td>
-                <td><h2>{{ item.prediction + ' ' + (item.prediction === '1' ? 'minute' : 'minutes') }}</h2></td>
+                <td style="text-align: left;"><h3>{{ item.title }}</h3></td>
+                <td><h3>{{ item.prediction + ' ' + (item.prediction === '1' ? 'minute' : 'minutes') }}</h3></td>
               </tr>
             </tbody>
           </table>
+        </div>
+        <div>
+          <h1>Coming Up <span class="material-icons">schedule</span></h1>
+          <h2>{{ `${upcoming.title}: ${new Date(upcoming.start).toLocaleString()}` }}</h2>
         </div>
       </div>
     </div>
@@ -50,6 +54,9 @@ import './app.css'
 export default class App extends Vue {
   private currentAnnouncement: {type: "image" | "text", data: string} = { type: "text", data: 'hello!'};
   private currentAnnouncementIndex: number = 0;
+  private announcements: Array<{type: "image" | "text", data: string}> = [];
+  private events: Array<{start: string, end: string, location: string, title: string}> = [];
+  private upcoming: {start: string, end: string, location: string, title: string} = {start: '', end: '', location: '', title: ''};
   private error: boolean = false
   private info: Map<string, { prediction: string, title: string }> = new Map()
   private time: string = ''
@@ -95,12 +102,24 @@ export default class App extends Vue {
     fetch('config.json').then((data) => {
       return data.json();
     }).then((data) => {
-      this.currentAnnouncementIndex = Math.floor(Math.random() * data.length);
-      this.currentAnnouncement = data[this.currentAnnouncementIndex];
+      this.events = data.events;
+      this.announcements = data.announcements;
+
+      let events = this.events.filter(event => (new Date(event.start) < new Date()) && (new Date() < new Date(event.end)));
+
+      for(let event of events){
+        this.announcements.push({type: "text", data: `Happening now: ${event.title} (${event.location})`})
+      }
+
+      let upcoming = this.events.filter(event => new Date() < new Date(event.start)).sort(event => new Date(event.start).valueOf()- new Date().valueOf());
+      this.upcoming = upcoming[0];
+      
+      this.currentAnnouncementIndex = Math.floor(Math.random() * this.announcements.length);
+      this.currentAnnouncement = this.announcements[this.currentAnnouncementIndex];
       setInterval(() => {
         this.currentAnnouncementIndex += 1;
-        this.currentAnnouncementIndex %= data.length;
-        this.currentAnnouncement = data[this.currentAnnouncementIndex];
+        this.currentAnnouncementIndex %= this.announcements.length;
+        this.currentAnnouncement = this.announcements[this.currentAnnouncementIndex];
       }, 10000)
     }).catch((error) => {
       console.log(error)
